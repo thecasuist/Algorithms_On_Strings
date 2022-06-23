e31830f79e888fd126e77edc205605985181b36e
Afzal Rao <50012451+thecasuist@users.noreply.github.com>
2022-06-23 12:53:19 +0000

    week 2 p1 submission

--- w2p1.cpp
+++ w2p1.cpp
@@ -1,13 +1,37 @@
-#include <bits/stdc++.h>
-using namespace std;
 
-void solve(string &s){
-    
+
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
+
+string BWT(const string& text) {
+  string result = "";
+
+  // write your code here
+  vector<string> m(text.size());
+  for (int i = 0; i < m.size(); i++) {
+  	m[i] = text.substr(i) + text.substr(0, i);
+  }
+
+  sort(m.begin(), m.end());
+
+  for (int i = 0; i < m.size(); i++) {
+  	result += m[i][m.size() - 1];
+  }
+
+  return result;
 }
 
-int main(){
-    string s;
-    cin >> s;
-    solve(s);
-    return 0;
+int main() {
+  string text;
+  cin >> text;
+  cout << BWT(text) << endl;
+  return 0;
 }
